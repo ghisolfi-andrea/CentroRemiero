dd41abb619a58eca37b68a78f206d29cb1b27a2b
ghisolfi-andrea <andreaghisolfi4@gmail.com>
2023-05-10 09:42:01 +0000

    ho aggiunto le pagine mancanti, ho aggiunto i cookies e le immagini

--- home.js
+++ home.js
@@ -1,75 +1,34 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000);
 
   
-
-// Funzione per mostrare il pop-up dei cookies
-function showCookiePopup() {
-  var popup = document.getElementById("cookie-popup");
-  popup.style.display = "block";
-}
-
-// Funzione per nascondere il pop-up dei cookies
-function hideCookiePopup() {
-  var popup = document.getElementById("cookie-popup");
-  popup.style.display = "none";
-}
-
-// Controllo se i cookies sono già stati accettati
-if (!getCookie("cookiesAccepted")) {
-  showCookiePopup();
-}
-
-// Aggiungo l'evento onclick al pulsante di accettazione dei cookies
-var acceptCookieButton = document.getElementById("accept-cookie");
-acceptCookieButton.onclick = function() {
-  setCookie("cookiesAccepted", true, 365);
-  hideCookiePopup();
-};
-
-// Funzione per impostare un cookie
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
   }
-  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-
-// Funzione per ottenere un cookie
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+ 
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
   }
-  return null;
-}
+ 
+  function closeCookiePopup() {
+    setCookie("cookiePopupShown", "true", 7);
+    document.getElementById("cookiePopup").style.display = "none";
+  }
+ 
+  window.onload = function () {
+    if (!getCookie("cookiePopupShown")) {
+      document.getElementById("cookiePopup").style.display = "block";
+    }
+  };
+
