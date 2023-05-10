9d1015a94a84aeb10f57cbd27599c643c22f1a7d
ghisolfi-andrea <andreaghisolfi4@gmail.com>
2023-05-10 11:00:12 +0000

    devo sistemare la comparsa dei testi

--- home.js
+++ home.js
@@ -1,42 +1,37 @@
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  return null;
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
 
-function acceptCookies() {
-  setCookie("cookiePopupShown", "true", 7);
-  document.getElementById("cookiePopup").style.display = "none";
+
+function mostraTesto1() {
+  document.getElementById("testo1").style.display = "block";
 }
 
-function rejectCookies() {
-  setCookie("cookiePopupShown", "false", 7);
-  document.getElementById("cookiePopup").style.display = "none";
+function mostraTesto2() {
+  document.getElementById("testo2").style.display = "block";
 }
-
-window.onload = function () {
-  if (getCookie("cookiePopupShown") !== "true") {
-    document.getElementById("cookiePopup").style.display = "block";
-  }
-  document.getElementById("cookieLink").addEventListener("click", function (e) {
-    e.preventDefault();
-    // Inserire qui il link alla pagina di informazioni sui cookie
-    alert("Pagina di informazioni sui cookie non disponibile.");
-  });
-};
-
